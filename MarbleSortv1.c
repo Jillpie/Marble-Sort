27c0b2377b2aeb84e8cfbb7b9566abdca5595838
Michael Leonffu <leonffu.michael@gmail.com>
2016-04-13 07:59:30 -0700

    Attempt to make a Average
    Useing Arrays, This is for the Values to find out if there are posible
    solutionsiwth certain sensors

---
+++ MarbleSortv1.c
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+task main(){
+	int values[100];
+	for(int i = 0; i < 100; i++){
+ 	 values[i] = SensorValue[light];
+	  wait1Msec(100);
+	}
+
+	int arrayTotal
+	float arrayAvg;
+	int array98 = values[98];
+	int array99 = values[99];
+
+	for(int p = 0; p < 100; p++){
+		arrayTotal =+ values[p];
+	}
+	arrayAvg = arrayTotal / 99;    //maybe its /100? 
+}
