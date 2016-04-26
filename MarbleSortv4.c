5c4a1590a15604a8dc6a47118e7fa2f807d3a886
Michael Leonffu <leonffu.michael@gmail.com>
2016-04-26 00:11:10 -0700

    Stared implementing final code for the program

--- MarbleSortv4.c
+++ MarbleSortv4.c
@@ -10,27 +10,40 @@
 
 //VARIBLES AND CONSTANTS:
 	//Task Main FLow Control and Debug:
-		int stage = 1;
+		bool stage = false;
 
 	//Planning Functions:
 		int SEAVALUE = 20;
+
 		//Solid Glass Plan Sea:
-			int marbleTypeLine;
+			bool marbleTypeLine;
 			int solidGlassSolid;
 			int solidGlassGlass;
-			int solidGlassFin = 0;
+			bool solidGlassFin = false;
 			int LINESOLID = 2500;
 			int LINETRESH = 50;
 
+		//Color Glass Plan Sea:
+			bool marbleColorLight;
+			int colorGlassGlass;
+			int colorGlassColor;
+			bool colorGlassFin = false;
+			int LIGHTCOLOR = 300;
+			int COLORTRESH = 5;
+
 	//Action Functions:
 		int ACTIONTIME = 4000;
-			//Servo Action:
-				int SERVOLETIN = -110;
-				int SERVOLETOUT = 110;
+
+		//Servo Action:
+			int SERVOLETIN = -110;
+			int SERVOLETOUT = 110;
 
 	//Other Functions:
 		//Pistion Action:
-			bool pistionAction = 0;
+			bool pistionStage = false;
+			int PISTIONMIN = 300;
+			int PISTIONMAX = 400;
+
 
 	//Universal Constants:
 		int POTFALLRIGHT = 650;
@@ -43,7 +56,7 @@
 
 //FUNCTIONS:
 	void solidGlassPlanSea(){
-		if(solidGlassFin == 0){
+		if(solidGlassFin == false){
 			if(SensorValue(sensorLine) < LINESOLID + LINETRESH){
 				solidGlassSolid ++;
 				solidGlassGlass = 0;
@@ -53,33 +66,33 @@
 				solidGlassGlass ++;
 			}
 			if(solidGlassGlass >= SEAVALUE){
-				solidGlassFin = 1;
-				marbleTypeLine = 2;
+				solidGlassFin = true;
+				marbleTypeLine = true;
 			}
 			if(solidGlassSolid >= SEAVALUE){
-				solidGlassFin = 1;
-				marbleTypeLine = 1;
+				solidGlassFin = true;
+				marbleTypeLine = false;
 			}
 		}
 	}
 
 	void colorGlassPlanSea(){
-		if(colorGlassFin == 0){
-			if(SensorValue(sensorLine) < LINESOLID + LINETRESH){
-				solidGlassSolid ++;
-				solidGlassGlass = 0;
+		if(colorGlassFin == false){
+			if(SensorValue(sensorLight) < LIGHTCOLOR + COLORTRESH){
+				colorGlassColor ++;
+				colorGlassGlass = 0;
 			}
-			if(SensorValue(sensorLine) > LINESOLID - LINETRESH){
-				solidGlassSolid = 0;
-				solidGlassGlass ++;
+			if(SensorValue(sensorLight) > LIGHTCOLOR - COLORTRESH){
+				colorGlassColor = 0;
+				colorGlassGlass ++;
 			}
-			if(solidGlassGlass >= SEAVALUE){
-				solidGlassFin = 1;
-				marbleTypeLine = 2;
+			if(colorGlassGlass >= SEAVALUE){
+				colorGlassFin = true;
+				marbleColorLight = true;
 			}
-			if(solidGlassSolid >= SEAVALUE){
-				solidGlassFin = 1;
-				marbleTypeLine = 1;
+			if(colorGlassColor >= SEAVALUE){
+				colorGlassFin = true;
+				marbleColorLight = false;
 			}
 		}
 	}
@@ -94,17 +107,23 @@
 	}
 
 	void pistionAction(){
-		if(pistionStage == 1){
-
+		if(pistionStage == true){
+			startMotor(pistionMotor,MOTORPOWER);
+			if(SensorValue(potPistion) > PISTIONMAX){
+				pistionStage = false;
+			}
 		}
-		if(pistionStage == 0){
-
+		if(pistionStage == false){
+			startMotor(pistionMotor,-MOTORPOWER);
+			if(SensorValue(potPistion) < PISTIONMIN){
+				pistionStage = true;
+			}
 		}
 	}
 
 	void solidGlassAction(){
 		if((time1(T1) > ((2 * ACTIONTIME)/4)) && (time1(T1) < ((3 * ACTIONTIME)/4))){
-			if(marbleTypeLine == 1){
+			if(marbleTypeLine == false){
 				if(SensorValue(potLine) > POTFALLRIGHT){
 					startMotor(lineMotor,MOTORPOWER);
 				}
@@ -112,7 +131,7 @@
 					startMotor(lineMotor,0);
 				}
 			}
-			if(marbleTypeLine == 2){
+			if(marbleTypeLine == true){
 				if(SensorValue(potLine) < POTFALLLEFT){
 					startMotor(lineMotor,-MOTORPOWER);
 				}
@@ -136,7 +155,7 @@
 
 	void colorGlassAction(){
 		if((time1(T1) > ((2 * ACTIONTIME)/4)) && (time1(T1) < ((3 * ACTIONTIME)/4))){
-			if(marbleTypeLine == 1){
+			if(marbleTypeLine == false){
 				if(SensorValue(potLine) > POTFALLRIGHT){
 					startMotor(lineMotor,MOTORPOWER);
 				}
@@ -144,7 +163,7 @@
 					startMotor(lineMotor,0);
 				}
 			}
-			if(marbleTypeLine == 2){
+			if(marbleTypeLine == true){
 				if(SensorValue(potLine) < POTFALLLEFT){
 					startMotor(lineMotor,-MOTORPOWER);
 				}
@@ -170,23 +189,23 @@
 task main(){
 	while(true){
 		pistionAction();
-		if(stage == 1){
+		if(stage == false){
 			solidGlassPlanSea();
 			colorGlassPlanSea();
 			clearTimer(T1);
-			if((solidGlassFin == 1) && (colorGlassFin == 1)){
-				stage = 2;
-				solidGlassFin = 0;
+			if((solidGlassFin == true) && (colorGlassFin == 1)){
+				stage = true;
+				solidGlassFin = false;
 				colorGlassFin = 0;
 			}
 		}
-		if(stage == 2){
+		if(stage == true){
 			servoAction();
 			solidGlassAction();
 			colorGlassAction();
 			if(time1(T1) > ACTIONTIME + 1000){
 				startMotor(lineMotor,0);
-				stage = 1;
+				stage = false;
 			}
 		}
 	}
